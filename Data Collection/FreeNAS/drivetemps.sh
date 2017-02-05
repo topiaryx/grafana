13c0d53cb608e9266ae919907c14ca0567baa61b
Tyler Hammer <tyler@tylerhammer.com>
2017-02-04 20:37:45 -0500

    Updated Config files

--- Data Collection/FreeNAS/drivetemps.sh
+++ Data Collection/FreeNAS/drivetemps.sh
@@ -7,13 +7,16 @@
 #Create a file named password where this script will run from and enter your FreeNAS root password into it, this will be
 #Needed for sshpass to connect using -f password , as password is the file name
 
+# Configuration File Location
+. drivetemps.cfg
+
 #Prepare to start the loop and warn the user
 echo "Press [CTRL+C] to stop..."
 
 while :
 do
 #Connect to FreeNAS
-    connect=$(sshpass -f /path/to/password/file ssh -oStrictHostKeyChecking=no USER@ESXiIP "/bin/sh /root/scripts/hdd_temp.sh" >/dev/null)
+    connect=$(shpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -t $USER@$ESXIP "/bin/sh /root/scripts/hdd_temp.sh" >/dev/null)
 #Wait for a bit before checking again
 sleep 15m
 done
--- Data Collection/FreeNAS/hdd_temp.sh
+++ Data Collection/FreeNAS/hdd_temp.sh
@@ -4,19 +4,16 @@
 
 ##This script MUST be placed into /root/scripts/ and named hdd_temp.sh , if the folder does not exist, CREATE IT
 
-
-drives="INSERT DRIVES daX or adaX"
-
-#The time we are going to sleep between readings
-#sleeptime=900
+# Configuration File Location
+. hdd_temp.cfg
 
  ###### summary ######
 ### Disks ###
-        for drive in $drives
+        for drive in $DRIVES
         do
           temp="$(smartctl -A /dev/${drive} | grep "Temperature_Celsius" | awk '{print $10}')"
-          curl -i -XPOST 'http://INFLUXDBIP:8086/write?db=home' --data-binary "health_data,host=<HOSTNAME>,sensor=$drive value=$temp"
+          curl -i -XPOST "http://$INFLUXIP/write?db=$DATABASE" --data-binary "health_data,host=$HOST,sensor=$drive value=$temp"
         done
 
 #Wait for a bit before checking again
-#sleep "$sleeptime"
+#sleep "$INTERVAL"
