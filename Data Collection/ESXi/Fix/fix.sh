0b0a94573e41bd9b67628928bac212d3a73b0ba0
Tyler Hammer <tyler@tylerhammer.com>
2017-02-06 20:27:32 -0500

    Update

--- Data Collection/ESXi/Fix/fix.sh
+++ Data Collection/ESXi/Fix/fix.sh
@@ -80,3 +80,4 @@ clear
 
 echo -e "\e[7mThe fix should now be applied, and you should be able to restart your esxi script. \e[0m"
 echo -e "\e[7mIf you're still having trouble please reach out to me git@tylerhammer.com or on Discord Hammer#4341. \e[0m"
+echo 
